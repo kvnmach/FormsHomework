7a477e562e0c45c1854fb30611c19e403693c26f
kvnmach <kvnmach@gmail.com>
2016-04-07 13:52:45 -0500

    added a list of task and initial setup for post

--- FormsHomework/Controllers/TaskController.cs
+++ FormsHomework/Controllers/TaskController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FormsHomework.Models;
 
 namespace FormsHomework.Controllers
 {
@@ -11,7 +12,21 @@ namespace FormsHomework.Controllers
         // GET: Task
         public ActionResult Index()
         {
+            List<Task> tasks;
+            tasks = (List<Task>) Session["tasks"] ?? new List<Task>();
+            return View(tasks);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Task newTask)
+        {
+            
         }
     }
 }
