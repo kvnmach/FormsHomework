4c51351443fbd55a5cfb3c4cb1cb3d488d45041b
kvnmach <kvnmach@gmail.com>
2016-04-07 13:44:44 -0500

    created class Task in model

---
+++ FormsHomework/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormsHomework.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public string Information { get; set; }
+
+    public Task(int id, string name, string info, bool isDone)
+        {
+            Id = id;
+            Name = name;
+            Information = info;
+            IsDone = isDone;
+        }
+
+    }
+}
